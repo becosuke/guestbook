ae97b012dd2c2cf126e6973357e40cb05354dc43
becosuke <becosuke@gmail.com>
2023-02-14 01:18:15 +0900

    grpc validator

--- api/internal/drivers/grpcserver/grpcserver.go
+++ api/internal/drivers/grpcserver/grpcserver.go
@@ -1,10 +1,11 @@
 package grpcserver
 
 import (
-	"github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
 	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
 	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
+	grpc_validator "github.com/grpc-ecosystem/go-grpc-middleware/validator"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 )
@@ -14,11 +15,13 @@ func NewGrpcServer(logger *zap.Logger, authFunc grpc_auth.AuthFunc) *grpc.Server
 		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
 			grpc_zap.StreamServerInterceptor(logger),
 			grpc_auth.StreamServerInterceptor(authFunc),
+			grpc_validator.StreamServerInterceptor(),
 			grpc_recovery.StreamServerInterceptor(),
 		)),
 		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
 			grpc_zap.UnaryServerInterceptor(logger),
 			grpc_auth.UnaryServerInterceptor(authFunc),
+			grpc_validator.UnaryServerInterceptor(),
 			grpc_recovery.UnaryServerInterceptor(),
 		)),
 	)
