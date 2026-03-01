961740f4903b14b9316e8aa09914277baf554495
becosuke <4677773+becosuke@users.noreply.github.com>
2026-03-01 20:43:52 +0900

    refactor: merge injector into main for grpc and rest commands

--- api/internal/cmd/grpc/main.go
+++ api/internal/cmd/grpc/main.go
@@ -10,10 +10,17 @@ import (
 
 	"go.uber.org/zap"
 	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
-	pkgconfig "github.com/becosuke/guestbook/api/internal/pkg/config"
+	"github.com/becosuke/guestbook/api/internal/adapter/controller"
+	repository_postgres "github.com/becosuke/guestbook/api/internal/adapter/repository/postgres"
+	"github.com/becosuke/guestbook/api/internal/driver/grpcserver"
+	"github.com/becosuke/guestbook/api/internal/driver/interceptor"
+	"github.com/becosuke/guestbook/api/internal/pkg/config"
+	"github.com/becosuke/guestbook/api/internal/pkg/logger"
 	"github.com/becosuke/guestbook/api/internal/pkg/pb"
+	"github.com/becosuke/guestbook/api/internal/usecase"
 )
 
 const (
@@ -26,6 +33,34 @@ var (
 	version     string
 )
 
+type App struct {
+	Config     *config.Config
+	Logger     *zap.Logger
+	GrpcServer *grpc.Server
+	Controller pb.GuestbookServiceServer
+}
+
+func InitializeApp(ctx context.Context) *App {
+	cfg := config.NewConfig(ctx)
+	zapLogger := logger.NewLogger(ctx, cfg)
+	authFunc := interceptor.NewAuthFunc(ctx)
+	server := grpcserver.NewGrpcServer(ctx, zapLogger, authFunc)
+	pool, err := repository_postgres.NewPool(ctx, cfg.DatabaseURL)
+	if err != nil {
+		zapLogger.Fatal("failed to connect to database", zap.Error(err))
+	}
+	querier := repository_postgres.NewQuerier(cfg, zapLogger, pool)
+	commander := repository_postgres.NewCommander(cfg, zapLogger, pool)
+	uc := usecase.NewUsecase(cfg, zapLogger, querier, commander)
+	ctrl := controller.NewGuestbookServiceServer(cfg, zapLogger, uc)
+	return &App{
+		Config:     cfg,
+		Logger:     zapLogger,
+		GrpcServer: server,
+		Controller: ctrl,
+	}
+}
+
 func main() {
 	os.Exit(run())
 }
@@ -34,28 +69,28 @@ func run() int {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	ctx = context.WithValue(ctx, pkgconfig.ServiceName{}, serviceName)
-	ctx = context.WithValue(ctx, pkgconfig.ServiceVersion{}, version)
+	ctx = context.WithValue(ctx, config.ServiceName{}, serviceName)
+	ctx = context.WithValue(ctx, config.ServiceVersion{}, version)
 
 	app := InitializeApp(ctx)
-	config := app.Config
-	logger := app.Logger
+	cfg := app.Config
+	zapLogger := app.Logger
 	defer func() {
-		_ = logger.Sync()
+		_ = zapLogger.Sync()
 	}()
 
 	grpcServer := app.GrpcServer
-	controller := app.Controller
-	pb.RegisterGuestbookServiceServer(grpcServer, controller)
+	ctrl := app.Controller
+	pb.RegisterGuestbookServiceServer(grpcServer, ctrl)
 	reflection.Register(grpcServer)
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, syscall.SIGTERM, os.Interrupt)
 	defer signal.Stop(interrupt)
 
-	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", config.GrpcHost, config.GrpcPort))
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", cfg.GrpcHost, cfg.GrpcPort))
 	if err != nil {
-		logger.Error("grpc server: failed to listen", zap.Error(err))
+		zapLogger.Error("grpc server: failed to listen", zap.Error(err))
 		return exitError
 	}
 
@@ -63,22 +98,22 @@ func run() int {
 	eg.Go(func() error {
 		return grpcServer.Serve(listener)
 	})
-	logger.Info("grpc server: serving", zap.String("host", config.GrpcHost), zap.Int("port", config.GrpcPort))
+	zapLogger.Info("grpc server: serving", zap.String("host", cfg.GrpcHost), zap.Int("port", cfg.GrpcPort))
 
 	select {
 	case <-interrupt:
-		logger.Info("received shutdown signal")
+		zapLogger.Info("received shutdown signal")
 	case <-ctx.Done():
-		logger.Info("context canceled")
+		zapLogger.Info("context canceled")
 	}
 	cancel()
 
-	logger.Info("grpc server: going gracefully shutdown")
+	zapLogger.Info("grpc server: going gracefully shutdown")
 	grpcServer.GracefulStop()
-	logger.Info("grpc server: completed gracefully shutdown")
+	zapLogger.Info("grpc server: completed gracefully shutdown")
 
 	if err := eg.Wait(); err != nil {
-		logger.Error("received error", zap.Error(err))
+		zapLogger.Error("received error", zap.Error(err))
 		return exitError
 	}
 
